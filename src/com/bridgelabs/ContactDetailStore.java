0fafb27fd74cb8a33d33cd94e8fe70d545e3f2e5
mukulBillore <mukulbillore88@gmail.com>
2021-12-20 18:03:27 +0530

    modified : modified the class name from AddressBookVariables to ContectDetailsStore

---
+++ src/com/bridgelabs/ContactDetailStore.java
@@ -0,0 +1,13 @@
+package com.bridgelabs;
+
+public class ContactDetailStore {
+	String Book;
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	int  zip;
+	int phonenumber ;
+	String email;
+}
