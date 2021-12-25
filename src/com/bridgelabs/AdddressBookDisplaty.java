16b1dff1b064c12f72249b4f8dc8a82cc7f0c925
mukulBillore <mukulbillore88@gmail.com>
2021-12-25 19:58:54 +0530

    Added :uc2 Ablity to add a person is added

--- src/com/bridgelabs/AdddressBookDisplaty.java
+++ src/com/bridgelabs/AdddressBookDisplaty.java
@@ -1,12 +1,28 @@
 package com.bridgelabs;
 
+import java.util.Scanner;
+
 public class AdddressBookDisplaty {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter details seperated by enter in this sequance :[Book, firstName, lastName, address, city, state , email,zip, phonenumber]:");
+
 		
-		System.out.println("welcome to address book displaying program :");
+		String bookName = sc.nextLine();
+		String firstName = sc.nextLine();
+		String lastName = sc.nextLine();
+		String address = sc.nextLine();
+		String city = sc.nextLine();
+		String state = sc.nextLine();
+		String email = sc.nextLine();
+		sc.nextLine();
+		int zip = sc.nextInt();
+		double number = sc.nextDouble();
 		
-	
+
+		ContactDetailStore mukul = new ContactDetailStore(bookName, firstName, lastName, address, city, state, zip, number, email);
+		System.out.println(mukul);
 	}
 
 }
--- src/com/bridgelabs/ContactDetailStore.java
+++ src/com/bridgelabs/ContactDetailStore.java
@@ -8,6 +8,26 @@ public class ContactDetailStore {
 	String city;
 	String state;
 	int  zip;
-	int phonenumber ;
+	double phonenumber ;
+	@Override
+	public String toString() {
+		return "ContactDetailStore [Book=" + Book + ", firstName=" + firstName + ", lastName=" + lastName + ", address="
+				+ address + ", city=" + city + ", state=" + state + ", zip=" + zip + ", phonenumber=" + phonenumber
+				+ ", email=" + email + "]";
+	}
 	String email;
+	public ContactDetailStore(String book, String firstName, String lastName, String address, String city, String state,
+			int zip, double phonenumber, String email) {
+		super();
+		Book = book;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phonenumber = phonenumber;
+		this.email = email;
+	}
+	
 }
